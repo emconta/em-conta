aff3d3a225b1ba6799fe6bb609ad211f737c0f17
kaleb <cardtunic@gmail.com>
2026-06-27 10:55:22 -0300

    feat(web): add api integration with hono client

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@web/components/ui/button";
+import { api } from "@web/lib/api";
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    api.health.$get().then((res) => res.text().then((text) => console.log));
+  }, []);
+
   return (
     <main className="w-screen h-screen flex items-center justify-center">
       <Button>Butão</Button>
